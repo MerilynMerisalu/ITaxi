e4475240be9be066ee2097c5e26e955640bc57f7
Merilyn Merisalu <175918@taltech.ee>
2022-04-06 23:12:42 +0300

    Changed the db to use docker

--- ITaxi/ITaxi/App.DAL.EF/AppDbContextFactory.cs
+++ ITaxi/ITaxi/App.DAL.EF/AppDbContextFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
 namespace App.DAL.EF;
@@ -8,8 +9,9 @@ public class  AppDbContextFactory: IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseSqlServer("Server=DESKTOP-49FEC6K;Database=ITaxiDb;User Id=sa;Password=Hobujaama10;");
-
+        optionsBuilder.UseSqlServer("Server=localhost,1433; Database=ITaxiDb;User=sa;Password=Hobujaama10;");
         return new AppDbContext(optionsBuilder.Options);
     }
+
+    
 }
