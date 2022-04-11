d1f935b5a912106d2ac14c963595bc99042f9ebc
Merilyn Merisalu <175918@taltech.ee>
2022-04-12 00:44:28 +0300

    Unused directives removed!

--- ITaxi/ITaxi/WebApp/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ ITaxi/ITaxi/WebApp/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -2,13 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 using App.DAL.EF;
-using App.Domain;
 using App.Domain.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
