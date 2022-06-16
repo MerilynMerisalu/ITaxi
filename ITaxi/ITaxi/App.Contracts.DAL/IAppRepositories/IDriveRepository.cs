8f4a946f24b0308f6350865fdb1b5dc3005b46cd
Merilyn Merisalu <175918@taltech.ee>
2022-06-16 20:24:44 +0300

    Implementing drive uow

--- ITaxi/ITaxi/App.Contracts.DAL/IAppRepositories/IDriveRepository.cs
+++ ITaxi/ITaxi/App.Contracts.DAL/IAppRepositories/IDriveRepository.cs
@@ -5,5 +5,18 @@ namespace App.Contracts.DAL.IAppRepositories;
 
 public interface IDriveRepository: IEntityRepository<Drive>
 {
-    // Add custom methods here
+    Task<IEnumerable<Drive>> GetAllDrivesWithoutIncludesAsync(bool noTracking = true);
+    IEnumerable<Drive> GetAllDrivesWithoutIncludes(bool noTracking = true);
+    Task<IEnumerable<Drive>> GettingAllOrderedSchedulesWithIncludesAsync(bool noTracking = true);
+    IEnumerable<Drive> GettingAllOrderedDrivesWithIncludes(bool noTracking = true);
+    Task<IEnumerable<Drive>> GettingAllOrderedSchedulesWithoutIncludesAsync(bool noTracking = true);
+    IEnumerable<Drive> GettingAllOrderedDrivesWithoutIncludes(bool noTracking = true);
+    Task<Drive?> GettingDriveWithoutIncludesAsync(Guid id, bool noTracking = true);
+    Drive? GetDriveWithoutIncludes(Guid id, bool noTracking = true);
+    Task<IEnumerable<Drive?>> SearchByDateAsync(DateTime search);
+    IEnumerable<Drive?> SearchByDate(DateTime search);
+
+    Task<IEnumerable<Drive?>> PrintAsync();
+    IEnumerable<Drive?>Print();
+    
 }
--- ITaxi/ITaxi/App.DAL.EF/Repositories/DriveRepository.cs
+++ ITaxi/ITaxi/App.DAL.EF/Repositories/DriveRepository.cs
@@ -9,4 +9,89 @@ public class DriveRepository: BaseEntityRepository<Drive, AppDbContext>, IDriveR
     public DriveRepository(AppDbContext dbContext) : base(dbContext)
     {
     }
+
+    protected override IQueryable<Drive> CreateQuery(bool noTracking = true)
+    {
+        return base.CreateQuery(noTracking);
+    }
+
+    public override Task<IEnumerable<Drive>> GetAllAsync(bool noTracking = true)
+    {
+        return base.GetAllAsync(noTracking);
+    }
+
+    public override IEnumerable<Drive> GetAll(bool noTracking = true)
+    {
+        return base.GetAll(noTracking);
+    }
+
+    public override Task<Drive?> FirstOrDefaultAsync(Guid id, bool noTracking = true)
+    {
+        return base.FirstOrDefaultAsync(id, noTracking);
+    }
+
+    public override Drive? FirstOrDefault(Guid id, bool noTracking = true)
+    {
+        return base.FirstOrDefault(id, noTracking);
+    }
+
+    public async Task<IEnumerable<Drive>> GetAllDrivesWithoutIncludesAsync(bool noTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Drive> GetAllDrivesWithoutIncludes(bool noTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Drive>> GettingAllOrderedSchedulesWithIncludesAsync(bool noTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Drive> GettingAllOrderedDrivesWithIncludes(bool noTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Drive>> GettingAllOrderedSchedulesWithoutIncludesAsync(bool noTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Drive> GettingAllOrderedDrivesWithoutIncludes(bool noTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Drive?> GettingDriveWithoutIncludesAsync(Guid id, bool noTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Drive? GetDriveWithoutIncludes(Guid id, bool noTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Drive?>> SearchByDateAsync(DateTime search)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Drive?> SearchByDate(DateTime search)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Drive?>> PrintAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Drive?> Print()
+    {
+        throw new NotImplementedException();
+    }
 }
