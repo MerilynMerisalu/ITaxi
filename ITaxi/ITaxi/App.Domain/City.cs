d34b2cbabb254d890b49c991514697c35689d542
Merilyn Merisalu <175918@taltech.ee>
2022-06-26 19:17:46 +0300

    City index translated!

--- ITaxi/ITaxi/App.Domain/City.cs
+++ ITaxi/ITaxi/App.Domain/City.cs
@@ -1,20 +1,22 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
 using Base.Domain;
 
 namespace App.Domain
 {
     public class City : DomainEntityMetaId
     {
-        
-        [DisplayName("County Name")] 
         public Guid CountyId { get; set; }
 
+        [Display(ResourceType = typeof(App.Resources.Areas.App.Domain.AdminArea.City),
+        Name = "CountyName")] 
         public County? County { get; set; }
 
         [Required]
         [MaxLength(50)]
-        [DisplayName("City")]
+        [Display(ResourceType = typeof(App.Resources.Areas.App.Domain.AdminArea.City),
+            Name = nameof(CityName))]
         public string CityName { get; set; } = default!;
     }
 }
