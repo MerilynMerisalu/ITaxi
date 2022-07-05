18812d7f04d0fef55bbfb827e37916eae4ecf507
Merilyn Merisalu <175918@taltech.ee>
2022-07-05 18:26:06 +0300

    Translating vehicle type views!

--- ITaxi/ITaxi/App.Domain/VehicleType.cs
+++ ITaxi/ITaxi/App.Domain/VehicleType.cs
@@ -1,15 +1,17 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using Base.Domain;
+using Base.Resources;
 
 namespace App.Domain;
 
 public class VehicleType: DomainEntityMetaId
 {
-    [Required]
-    [MaxLength(50)]
-    [StringLength(50, MinimumLength = 1)]
-    [DisplayName("Vehicle Type")]
+    [Required(ErrorMessageResourceType = typeof(Common), ErrorMessageResourceName = "RequiredAttributeErrorMessage")]
+    [MaxLength(50, ErrorMessageResourceType = typeof(Common), ErrorMessageResourceName = "ErrorMessageStringLengthMax")]
+    [StringLength(50, MinimumLength = 1, ErrorMessageResourceType = typeof(Common),
+        ErrorMessageResourceName = "StringLengthAttributeErrorMessage")]
+    [Display(ResourceType = typeof(Resources.Areas.App.Domain.AdminArea.VehicleType), Name = "VehicleTypeName")]
     public string VehicleTypeName { get; set; } = default!;
 
     public ICollection<Vehicle>? Vehicles { get; set; }
