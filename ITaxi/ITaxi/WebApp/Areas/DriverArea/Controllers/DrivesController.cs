143f6222f7684eb503f18501d6cf7389957dfb13
Merilyn Merisalu <175918@taltech.ee>
2022-07-28 03:02:13 +0300

    Created drive views and resx files for admin and driver areas

---
+++ ITaxi/ITaxi/WebApp/Areas/DriverArea/Controllers/DrivesController.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using App.DAL.EF;
+using App.Domain;
+
+namespace WebApp.Areas.DriverArea.Controllers
+{
+    [Area("DriverArea")]
+    public class DrivesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public DrivesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DriverArea/Drives
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Drives.Include(d => d.Driver);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: DriverArea/Drives/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Drives == null)
+            {
+                return NotFound();
+            }
+
+            var drive = await _context.Drives
+                .Include(d => d.Driver)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (drive == null)
+            {
+                return NotFound();
+            }
+
+            return View(drive);
+        }
+
+        // GET: DriverArea/Drives/Create
+        public IActionResult Create()
+        {
+            ViewData["DriverId"] = new SelectList(_context.Drivers, "Id", "Address");
+            return View();
+        }
+
+        // POST: DriverArea/Drives/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DriverId,CreatedBy,CreatedAt,UpdatedBy,UpdatedAt,Id")] Drive drive)
+        {
+            if (ModelState.IsValid)
+            {
+                drive.Id = Guid.NewGuid();
+                _context.Add(drive);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DriverId"] = new SelectList(_context.Drivers, "Id", "Address", drive.DriverId);
+            return View(drive);
+        }
+
+        // GET: DriverArea/Drives/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Drives == null)
+            {
+                return NotFound();
+            }
+
+            var drive = await _context.Drives.FindAsync(id);
+            if (drive == null)
+            {
+                return NotFound();
+            }
+            ViewData["DriverId"] = new SelectList(_context.Drivers, "Id", "Address", drive.DriverId);
+            return View(drive);
+        }
+
+        // POST: DriverArea/Drives/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("DriverId,CreatedBy,CreatedAt,UpdatedBy,UpdatedAt,Id")] Drive drive)
+        {
+            if (id != drive.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(drive);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DriveExists(drive.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DriverId"] = new SelectList(_context.Drivers, "Id", "Address", drive.DriverId);
+            return View(drive);
+        }
+
+        // GET: DriverArea/Drives/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Drives == null)
+            {
+                return NotFound();
+            }
+
+            var drive = await _context.Drives
+                .Include(d => d.Driver)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (drive == null)
+            {
+                return NotFound();
+            }
+
+            return View(drive);
+        }
+
+        // POST: DriverArea/Drives/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Drives == null)
+            {
+                return Problem("Entity set 'AppDbContext.Drives'  is null.");
+            }
+            var drive = await _context.Drives.FindAsync(id);
+            if (drive != null)
+            {
+                _context.Drives.Remove(drive);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DriveExists(Guid id)
+        {
+          return (_context.Drives?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
