c74b8ba44899a05896ea64e1658dc6a6ae259e86
Merilyn Merisalu <175918@taltech.ee>
2022-10-27 21:18:11 +0300

    Added customer to a role on customer registration!

--- ITaxi/ITaxi/WebApp/Areas/Identity/Pages/Account/RegisterCustomer.cshtml.cs
+++ ITaxi/ITaxi/WebApp/Areas/Identity/Pages/Account/RegisterCustomer.cshtml.cs
@@ -127,6 +127,8 @@ public class RegisterCustomerModel : PageModel
                 await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl!)}'>clicking here</a>.");
 
+                await _userManager.AddToRoleAsync(user, "Customer");
+                await _context.SaveChangesAsync();
                 var customer = new Customer
                 {
                     AppUserId = user.Id, DisabilityTypeId = Input.DisabilityTypeId
