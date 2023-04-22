09c73068d34a7a807a74121334d60e6d3727f921
Merilyn Merisalu <175918@taltech.ee>
2023-04-23 00:51:44 +0300

    Packages updated!

---
+++ ITaxi/ITaxi/WebApp.Testing/IntegrationTestsHomeController.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApp.Testing;
+
+public class IntegrationTestsHomeController
+{
+    private readonly HttpClient _client;
+
+    public IntegrationTestsHomeController(HttpClient client)
+    {
+        _client = client;
+    }
+
+    [Fact]
+
+    public async Task Get_Index()
+    {
+        // Arrange
+        // Act
+        var response = await _client.GetAsync("/");
+        
+        // Assert
+
+        response.EnsureSuccessStatusCode();
+    }
+}
