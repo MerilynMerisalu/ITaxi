00947048a44e88cfcdb0f3b4542efe119ad0cd99
Merilyn Merisalu <175918@taltech.ee>
2023-04-30 18:00:43 +0300

    small fixes

--- itaxi-app/src/components/Footer.tsx
+++ itaxi-app/src/components/Footer.tsx
@@ -2,8 +2,8 @@ const Footer = () => {
   return (
     <footer className="border-top footer text-muted">
       <div className="container">
-        &copy; 2023 - WebApp - <a href="/Home/Privacy">Privacy</a>| UI Culture:
-        en-GB (en-GB); | Culture: en-GB (en-GB);
+        &copy; 2023 - WebApp - <a href="/Privacy">Privacy</a> | UI Culture:
+        en-GB (en-GB) | Culture: en-GB (en-GB)
       </div>
     </footer>
   );
