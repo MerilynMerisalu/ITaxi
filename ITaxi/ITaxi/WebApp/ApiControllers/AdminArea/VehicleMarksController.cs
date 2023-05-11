e3f6b0da871b8d687e35d1f0761a46f083be2490
Merilyn Merisalu <mmeris@taltech.ee>
2023-05-12 02:05:58 +0300

    Vehicle marks api done!

--- ITaxi/ITaxi/WebApp/ApiControllers/AdminArea/VehicleMarksController.cs
+++ ITaxi/ITaxi/WebApp/ApiControllers/AdminArea/VehicleMarksController.cs
@@ -1,6 +1,9 @@
 #nullable enable
 using App.BLL.DTO.AdminArea;
 using App.Contracts.BLL;
+using App.Public.DTO.v1.AdminArea;
+using AutoMapper;
+using Base.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -15,40 +18,70 @@ namespace WebApp.ApiControllers.AdminArea;
 public class VehicleMarksController : ControllerBase
 {
     private readonly IAppBLL _appBLL;
-
-    public VehicleMarksController(IAppBLL appBLL)
+    private readonly IMapper _mapper;
+    public VehicleMarksController(IAppBLL appBLL, IMapper mapper)
     {
         _appBLL = appBLL;
+        _mapper = mapper;
     }
 
     // GET: api/VehicleMarks
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<VehicleMarkDTO>>> GetVehicleMarks()
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [ProducesResponseType( typeof( IEnumerable<VehicleMark>), StatusCodes.Status200OK )] 
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult<IEnumerable<VehicleMark>>> GetVehicleMarks()
     {
-        return Ok(await _appBLL.VehicleMarks.GetAllVehicleMarkOrderedAsync());
+
+        var res = await _appBLL.VehicleMarks.GetAllVehicleMarkOrderedAsync();
+        return Ok(res.Select(v => _mapper.Map<VehicleMark>(v)));
     }
 
     // GET: api/VehicleMarks/5
     [HttpGet("{id}")]
-    public async Task<ActionResult<VehicleMarkDTO>> GetVehicleMark(Guid id)
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [ProducesResponseType(typeof(VehicleMark), StatusCodes.Status200OK )] 
+    [ProducesResponseType( StatusCodes.Status404NotFound )] 
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult<VehicleMark>> GetVehicleMark(Guid id)
     {
         var vehicleMark = await _appBLL.VehicleMarks.FirstOrDefaultAsync(id);
 
         if (vehicleMark == null) return NotFound();
 
-        return vehicleMark;
+        return _mapper.Map<VehicleMark>(vehicleMark);
     }
 
     // PUT: api/VehicleMarks/5
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPut("{id}")]
-    public async Task<IActionResult> PutVehicleMark(Guid id, VehicleMarkDTO vehicleMark)
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> PutVehicleMark(Guid id, VehicleMark vehicleMark)
     {
         if (id != vehicleMark.Id) return BadRequest();
 
+        var vehicleMarkDto = await _appBLL.VehicleMarks.FirstOrDefaultAsync(id);
+        
         try
         {
-            _appBLL.VehicleMarks.Update(vehicleMark);
+            if (vehicleMarkDto != null)
+            {
+                vehicleMarkDto.VehicleMarkName = vehicleMark.VehicleMarkName;
+                vehicleMarkDto.UpdatedBy = User.GettingUserEmail();
+                vehicleMarkDto.UpdatedAt = DateTime.Now.ToUniversalTime();
+            }
+
+            if (vehicleMarkDto != null) _appBLL.VehicleMarks.Update(vehicleMarkDto);
             await _appBLL.SaveChangesAsync();
         }
         catch (DbUpdateConcurrencyException)
@@ -64,13 +97,20 @@ public class VehicleMarksController : ControllerBase
     // POST: api/VehicleMarks
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
     [HttpPost]
-    public async Task<ActionResult<VehicleMarkDTO>> PostVehicleMark([FromBody]VehicleMarkDTO vehicleMark)
+    public async Task<ActionResult<VehicleMark>> PostVehicleMark([FromBody]VehicleMark vehicleMark)
     {
         if (HttpContext.GetRequestedApiVersion() == null)
         {
             return BadRequest("Api version is mandatory");
         }
-        _appBLL.VehicleMarks.Add(vehicleMark);
+
+        var vehicleMarkDto = new VehicleMarkDTO();
+        vehicleMarkDto.VehicleMarkName = vehicleMark.VehicleMarkName;
+        vehicleMarkDto.CreatedBy = User.GettingUserEmail();
+        vehicleMarkDto.CreatedAt = DateTime.Now.ToUniversalTime();
+        vehicleMarkDto.UpdatedBy = User.GettingUserEmail();
+        vehicleMarkDto.UpdatedAt = DateTime.Now.ToUniversalTime();
+        _appBLL.VehicleMarks.Add(vehicleMarkDto);
         await _appBLL.SaveChangesAsync();
 
         return CreatedAtAction("GetVehicleMark", new
