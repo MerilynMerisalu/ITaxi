30d258893c609bf37a95a8036c0f0d1bccac414f
MMerisalu <merilyn.merisalu.01@gmail.com>
2023-12-16 20:09:20 +0200

    booking index

--- ITaxiClientAppBlazorSolution/App.Service/BookingService.cs
+++ ITaxiClientAppBlazorSolution/App.Service/BookingService.cs
@@ -22,7 +22,7 @@ namespace ITaxi.Service
         {
         }
 
-        protected override string EndpointUri => "/custumerarea/bookings/";
+        protected override string EndpointUri => "/customerArea/bookings/";
 
         public async Task<IEnumerable<Booking?>> GetAllBookingsAsync()
         {
