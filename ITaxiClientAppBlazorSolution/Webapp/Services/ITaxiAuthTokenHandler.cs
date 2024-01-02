28021022c0bd4fa31697097b9e98b62fbb5dfbaf
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-01-02 14:34:36 +0200

    Token validation moved!

--- ITaxiClientAppBlazorSolution/Webapp/Services/ITaxiAuthTokenHandler.cs
+++ ITaxiClientAppBlazorSolution/Webapp/Services/ITaxiAuthTokenHandler.cs
@@ -3,14 +3,15 @@ using Base.Service.Contracts;
 using BlazorWebApp.Services;
 using System.Net.Http.Json;
 using System.Net.Http;
+using Microsoft.Extensions.Http;
 
 
 namespace Webapp.Services
 {
-    public class ITaxiAuthTokenHandler(IAppState appState) : DelegatingHandler
+    public class ITaxiAuthTokenHandler(IAppState appState, IHttpClientFactory client) : DelegatingHandler
     {
         private readonly IAppState _appState = appState;
-
+        private readonly IHttpClientFactory _httpClient = client;
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
@@ -22,7 +23,24 @@ namespace Webapp.Services
             }
             return await base.SendAsync(request, cancellationToken);
         }
+        public async Task<AuthResponse> AuthenticateToken(string email, string password)
+        {
+            AuthResponse authResponse = new AuthResponse();
 
+            var client = _httpClient.CreateClient("Auth");
+
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+            var response = await client.PostAsJsonAsync($"{client.BaseAddress!.ToString().TrimEnd('/')}/identity/account/login", new { Email = email, Password = password });
+            if (response.IsSuccessStatusCode)
+            {
+                authResponse = await response.Content.ReadFromJsonAsync<AuthResponse>();
+
+            }
+
+            return authResponse!;
+        }
 
     }
 }
