0135969f5e2a853276e0943751d4df50b0fda213
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-01-18 18:02:17 +0200

    Added schedule list to ride times create.

---
+++ ITaxiClientAppBlazorSolution/Webapp/ViewModels/CreateRideTimeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Public.App.DTO.v1.DriverArea;
+
+namespace Webapp.ViewModels
+{
+    public class CreateRideTimeViewModel
+    {
+        public Guid ScheduleId { get; set; }
+        public Schedule? Schedule { get; set; }
+
+
+
+    }
+}
