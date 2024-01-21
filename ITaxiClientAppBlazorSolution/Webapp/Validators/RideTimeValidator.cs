62478b5fbe57ee911276aff28e4f5ae40100e091
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-01-21 18:00:12 +0200

    Schedule validator for a ride times in driver area.

--- ITaxiClientAppBlazorSolution/Webapp/Validators/RideTimeValidator.cs
+++ ITaxiClientAppBlazorSolution/Webapp/Validators/RideTimeValidator.cs
@@ -1,6 +1,14 @@
-﻿namespace Webapp.Validators
+﻿using FluentValidation;
+using Public.App.DTO.v1.DriverArea;
+using Webapp.ViewModels;
+
+namespace Webapp.Validators
 {
-    public class RideTimeValidator
+    public class RideTimeValidator: AbstractValidator<CreateRideTimeViewModel>
     {
+        public RideTimeValidator()
+        {
+            RuleFor(rt => rt.Schedule).NotNull();
+        }
     }
 }
