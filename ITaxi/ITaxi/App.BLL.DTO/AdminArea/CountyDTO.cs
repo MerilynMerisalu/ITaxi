669acf23a93cb6954c4142d8b86cb59ee0bfd0d2
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-03-23 23:10:16 +0200

    small fixes

--- ITaxi/ITaxi/App.BLL.DTO/AdminArea/CountyDTO.cs
+++ ITaxi/ITaxi/App.BLL.DTO/AdminArea/CountyDTO.cs
@@ -12,13 +12,10 @@ public class CountyDTO : DomainEntityMetaId
     [Display(ResourceType = typeof(Resources.Areas.App.Domain.AdminArea.County)
         , Name = nameof(CountyName))]
     public string CountyName { get; set; } = default!;
-
     public Guid CountryId { get; set; }
     [Display(ResourceType = typeof(Resources.Areas.App.Domain.AdminArea.County),
         Name = nameof(Country))]
     public CountryDTO? Country { get; set; }
-
-    
     
     public int NumberOfCities { get; set; }
 }
