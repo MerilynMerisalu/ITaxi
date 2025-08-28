bcb5f4695a9f10f08c190da07b0d786c14611951
MMerisalu <merilyn.merisalu.01@gmail.com>
2025-08-28 17:46:50 +0300

    Reorganized the code

--- ITaxi/ITaxi/WebApp/Areas/AdminArea/Controllers/PhotosController.cs
+++ ITaxi/ITaxi/WebApp/Areas/AdminArea/Controllers/PhotosController.cs
@@ -19,6 +19,7 @@ public class PhotosController : Controller
 {
     private readonly IAppBLL _appBLL;
     private readonly IWebHostEnvironment _webHostEnvironment;
+
     /// <summary>
     /// Admin area photos controller constructor
     /// </summary>
@@ -89,12 +90,12 @@ public class PhotosController : Controller
             photo.Id = Guid.NewGuid();
             photo.Title = vm.Title;
             photo.PhotoURL = vm.PhotoName;
-            
+
             _appBLL.Photos.Add(photo);
             await _appBLL.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-        
+
         return View(vm);
     }*/
 
@@ -111,7 +112,7 @@ public class PhotosController : Controller
 
         var photo = await _appBLL.Photos.FirstOrDefaultAsync(id.Value);
         if (photo == null) return NotFound();
-        
+
         return View(vm);
     }
 
@@ -147,7 +148,7 @@ public class PhotosController : Controller
 
             return RedirectToAction(nameof(Index));
         }
-        
+
         return View(vm);
     }*/
 
@@ -191,55 +192,114 @@ public class PhotosController : Controller
         return _appBLL.Photos.Exists(id);
     }*/
 
-    /*[AcceptVerbs("Post")]
-    public async Task<IActionResult> Upload(List<IFormFile>? files)
-    {*/
-        /*var filePaths = new List<string>();
+// GET: AdminArea/Vehicle/Gallery/5
+    /// <summary>
+    /// Admin area vehicle GET method gallery
+    /// </summary>
+    /// <param name="id">Id</param>
+    /// <returns>View</returns>
+    public async Task<IActionResult> VehiclesUpload(Guid? id)
+    {
+        if (id == null) return NotFound();
 
-        var firstFileName = Path.GetFileName(files.First().FileName);
-        var vehicleIdentifierParts = firstFileName.Split(" ");
-        var directoryName = string.Join(" ", vehicleIdentifierParts.Take(5));
+        var vm = new GalleryViewModel();
 
-        var filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Images", directoryName);
+        var vehicle = await _appBLL.Vehicles.GettingVehicleWithIncludesByIdAsync(id.Value);
+        if (vehicle == null) return NotFound();
 
-        if (!Directory.Exists(filePath))
-        {
-            Directory.CreateDirectory(filePath);
-        }
+        vm.VehicleIdentifier = vehicle.VehicleIdentifier;
+        vm.Id = vehicle.Id;
 
+        return View(vm);
+    }
+
+
+
+   
+    [AcceptVerbs("Post")]
+    public async Task<IActionResult> Upload([FromRoute] Guid id, List<IFormFile>? files)
+    {
+        var userRoleName = User.GettingUserRoleName();
+        string fileNameInDirectory = string.Empty;
+        var vehicle = await _appBLL.Vehicles.GettingVehicleWithIncludesByIdAsync(id);
+        if (vehicle == null) return NotFound();
+
+        if (files == null || files.Count == 0)
+            return Content("Images are required.");
+
+        int imagesAlreadyUploadedPerVehicle = await _appBLL.Photos.GetPhotoCountByVehicleIdAsync(
+            id, null, User.GettingUserRoleName(), true);
+
+        var result = _appBLL.Photos.AlreadyHasACertainNumberOfImages(files: files, numberOfImagesAllowed: 4,
+            numberOfImages: imagesAlreadyUploadedPerVehicle);
+
+        if (result)
+            return Content($"You can upload up to four images per vehicle.");
+        var driverId = await _appBLL.Vehicles.GetDriverIdByVehicleIdAsync(
+            id, null, User.GettingUserRoleName(), true, true);
+
+        if (driverId == null) return NotFound();
+
+        result = _appBLL.Photos.AreAllFilesCorrect(files);
+        string uploadFolderName = Guid.NewGuid().ToString();
+        if (result == false)
+            return Content("The image must be between 1 byte and 5MB and have a .png or " +
+                           ".jpg extension!");
         foreach (var file in files)
         {
-            if (file.Length > 0 && file.Length < 5000000 && file.FileName.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
+            int maxLength = 255;
+            string uploadFolderPath = "";
+            string fileName = _appBLL.Photos.FileNameFormat(fileName: file.FileName,
+                maxLength: maxLength);
+
+            if (result == false) throw new ArgumentException();
+
+            string? directoryId = await _appBLL.Photos.GetDirectoryIdByVehicleIdAsStringAsync(vehicle.Id,
+                null, userRoleName!);
+            if (directoryId == null)
             {
-                var fileName = Path.GetFileName(file.FileName);
-                var fullFilePath = Path.Combine(filePath, fileName);
+                string[]? directoryNames = { "Vehicles" };
+                uploadFolderPath = _appBLL.Photos.GetDirectoryPath(_webHostEnvironment.WebRootPath,
+                    directoryNames, uploadFolderName);
 
-                await using var stream = new FileStream(fullFilePath, FileMode.Create);
-                await file.CopyToAsync(stream);
+                bool isDirectoryCreated = _appBLL.Photos.DoesDirectoryExist(uploadFolderPath);
+                if (isDirectoryCreated == false)
+                    _appBLL.Photos.CreateDirectory(uploadFolderPath);
+                fileNameInDirectory = _appBLL.Photos.GetFileNameForDirectory(uploadFolderPath);
+                string imageRelativePath = Path.GetRelativePath(_webHostEnvironment.WebRootPath,
+                    Path.Combine(uploadFolderPath, fileNameInDirectory));
 
-                filePaths.Add(fullFilePath);
-            }*/
+                var photo = new PhotoDTO
+                {
+                    Id = Guid.NewGuid(),
+                    VehicleId = id,
+                    Title = fileName,
+                    PhotoURL = imageRelativePath,
+                    DriverId = driverId,
+                    DirectoryTitleId = uploadFolderName,
+                    FileNameInDirectory = fileNameInDirectory,
+                    CreatedBy = User.GettingUserEmail(),
+                    CreatedAt = DateTime.UtcNow,
+                    UpdatedBy = User.GettingUserEmail(),
+                    UpdatedAt = DateTime.UtcNow,
+                };
+
+                _appBLL.Photos.Add(photo);
+            }
+
+            await _appBLL.SaveChangesAsync();
+
+            if (_appBLL.Photos.DoesFileExist(uploadFolderPath!))
+                return Content("An image cannot be uploaded because its file already exists!");
+
+            if (await _appBLL.Photos.UploadImagesAsync(uploadFolderPath!, fileNameInDirectory, file) == false)
+                return Content("Upload failed");
 
         }
-       // return RedirectToAction("Gallery", "Vehicles");
 
-
-        /*var photoInfo = new PhotoDTO()
-        {
-            Id = Guid.NewGuid(),
-            Title = file.FileName,
-            AppUserId = User.GettingUserId(),
-            CreatedBy = User.GettingUserEmail(),
-            CreatedAt = DateTime.Now.ToUniversalTime(),
-            PhotoURL = filePath
-        };
-         _appBLL.Photos.Add(photoInfo);
-         await _appBLL.SaveChangesAsync();
-         */
-    //}
-      
-        
-    //}
+        return RedirectToAction("Gallery", "Vehicles" );
+    }
+}
 
 
 
