1b7babb39546970881f857d6016e9f57bab429ed
MMerisalu <merilyn.merisalu.01@gmail.com>
2025-09-23 15:06:59 +0300

    Changing views for vehicle gallery in admin area

--- ITaxi/ITaxi/WebApp/Areas/AdminArea/Controllers/VehiclesController.cs
+++ ITaxi/ITaxi/WebApp/Areas/AdminArea/Controllers/VehiclesController.cs
@@ -331,17 +331,29 @@ public class VehiclesController : Controller
     {
         return _appBLL.Vehicles.Exists(id);
     }
-    public async Task<IActionResult> Gallery(Guid id)
+    public async Task<IActionResult> ChooseView(Guid id)
     {
-        var vm = new VehicleGalleryAdminViewModel();
+        
         var roleName = User.GettingUserRoleName();
         var vehicle = await _appBLL.Vehicles.GettingVehicleWithIncludesByIdAsync(id, roleName: roleName);
         if (vehicle == null)
             return NotFound();
         
-        vm.Photos = await _appBLL.Photos.GetAllPhotosByVehicleIdWithIncludesAsync(vehicleId: vehicle.Id, roleName: roleName);
+        int numberOfPhotos = await _appBLL.Photos.GetPhotoCountByVehicleIdAsync(vehicle.Id, roleName: roleName);
+        if (numberOfPhotos == 0)
+        {
+            return RedirectToAction("VehicleImagesUpload", "Photos", new { id = vehicle.Id });
+        }
+        else
+        {
+            var vm = new VehicleGalleryAdminViewModel()
+            {
+                Photos = await _appBLL.Photos.GetAllPhotosByVehicleIdWithIncludesAsync(vehicle.Id)
+            };
+            return View("Gallery", vm);
+        }
 
-        return View(vm);
+        
     }
 
 }
