d5e148fc56af18c5a1c86d2cbc21d18e8565f5ea
MMerisalu <merilyn.merisalu.01@gmail.com>
2026-07-25 22:39:54 +0300

    Doing server side validation

---
+++ ITaxi/ITaxi/EstonianPersonalCode.Core/Enum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstonianPersonalCode.Core
+{
+    public enum EncodedSex
+    {
+        Female = 0,
+        Male = 1,
+        
+    } 
+
+    public enum EstonianPersonalCodeValidationError
+    {
+        None,
+        Empty,
+        InvalidLength,
+        ContainsNonDigits,
+        InvalidFirstDigit,
+        InvalidDate,
+        InvalidCheckDigit
+    }
+}
---
+++ ITaxi/ITaxi/EstonianPersonalCode.Core/EstonianPersonalCodeValidationResult.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace EstonianPersonalCode.Core
+{
+   public sealed class EstonianPersonalCodeValidationResult
+    {
+        
+        public bool IsValid { get; init; }
+        public DateOnly? DateOfBirth { get; init; }
+        public EncodedSex? EncodedSex { get; init; }
+
+        public EstonianPersonalCodeValidationError Error { get; init; }
+        private EstonianPersonalCodeValidationResult()
+        {
+            
+        }
+
+        public static EstonianPersonalCodeValidationResult Success(DateOnly dateOfBirth, 
+            EncodedSex encodedSex) 
+        {
+         
+            EstonianPersonalCodeValidationResult result = new EstonianPersonalCodeValidationResult()
+            { IsValid = true,
+              DateOfBirth = dateOfBirth,
+              EncodedSex = encodedSex,
+              Error = EstonianPersonalCodeValidationError.None
+            };
+
+            return result;
+        }
+        
+        public static EstonianPersonalCodeValidationResult Failure(EstonianPersonalCodeValidationError error)
+        {
+            if (error == EstonianPersonalCodeValidationError.None)
+            {
+                throw new ArgumentException("Error cannot be None when creating a failure result.", nameof(error));
+            }
+
+            EstonianPersonalCodeValidationResult result = new EstonianPersonalCodeValidationResult() {
+                IsValid = false,
+                Error = error,
+                EncodedSex = null,
+                DateOfBirth = null,
+            };
+            return result;
+        }
+
+
+    }
+
+    
+}
